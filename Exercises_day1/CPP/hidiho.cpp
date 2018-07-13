308b394912f25138e93bf9fdae5efdbf788743d2
ivy2@uchicago.edu <rccguest8455@midway254.rcc.local>
2018-07-12 22:41:51 -0500

    first bits

---
+++ Exercises_day1/CPP/hidiho.cpp
@@ -0,0 +1,14 @@
+//c++ program that takes name and prints it
+
+#include <iostream>
+#include <string>
+ 
+int main()
+{
+ using namespace std;
+ cout << "Write your name"; //print this
+ string x; //declare x as a string
+ cin >> x; //allow you to input your name
+ cout << "Hi" << x << endl; // print this
+ return 0;
+}
